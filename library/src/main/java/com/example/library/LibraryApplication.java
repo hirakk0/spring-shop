128f1d3742968f836bcefd143230fbce6703fa9d
ArekkusuSof <aleks82352@gmail.com>
2023-03-08 20:50:51 +0200

    Hello Maks

--- library/src/main/java/com/example/library/LibraryApplication.java
+++ library/src/main/java/com/example/library/LibraryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
-
+	//Тут був я
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
