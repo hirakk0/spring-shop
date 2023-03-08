ef8e0c7f6399846553c61809e1d6f06d0b82386d
ArekkusuSof <aleks82352@gmail.com>
2023-03-08 20:57:46 +0200

    I created new branch

--- library/src/main/java/com/example/library/LibraryApplication.java
+++ library/src/main/java/com/example/library/LibraryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
-	//Тут був я
+	//Тут був я(Arekkusu)
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
